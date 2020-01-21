3ab99eb0a8b65c4d7a884847e76a90be42c96815
cookienocreams <58923195+cookienocreams@users.noreply.github.com>
2020-01-20 19:41:33 -0600

    Added delta H + delta S calcs

--- PCR_Initial.py
+++ PCR_Initial.py
@@ -35,28 +35,39 @@ import re, math
 
 gas_constant = 1.98720425864083
 sequence = obtainfasta(chrID, start, stop)
+#Splits sequence into doublets for easy NN matching below
 duo_sequence = re.findall('.{1,2}', sequence) + re.findall('.{1,2}', sequence[1:])
 
-#Calculation of delta_G_total for each NN using data from van't Hoff script
-AA_NN_sum = AA_delta_g * sum([1.0 for i in duo_sequence if i in ['AA']])
-TT_NN_sum = AA_delta_g * sum([1.0 for i in duo_sequence if i in ['TT']])
-AT_NN_sum = AT_delta_g * sum([1.0 for i in duo_sequence if i in ['AT']])
-CA_NN_sum = CA_delta_g * sum([1.0 for i in duo_sequence if i in ['CA']])
-TG_NN_sum = CA_delta_g * sum([1.0 for i in duo_sequence if i in ['TG']])
-AC_NN_sum = CA_delta_g * sum([1.0 for i in duo_sequence if i in ['AC']])
-CG_NN_sum = CG_delta_g * sum([1.0 for i in duo_sequence if i in ['CG']])
-CT_NN_sum = CT_delta_g * sum([1.0 for i in duo_sequence if i in ['CT']])
-GA_NN_sum = GA_delta_g * sum([1.0 for i in duo_sequence if i in ['GA']])
-GC_NN_sum = GC_delta_g * sum([1.0 for i in duo_sequence if i in ['GC']])
-GT_NN_sum = GT_delta_g * sum([1.0 for i in duo_sequence if i in ['GT']]) 
-GG_NN_sum = GG_delta_g * sum([1.0 for i in duo_sequence if i in ['GG']])
-CC_NN_sum = GG_delta_g * sum([1.0 for i in duo_sequence if i in ['CC']])
-TA_NN_sum = TA_delta_g * sum([1.0 for i in duo_sequence if i in ['TA']])
+#Lists with all non-overlapping NN pair possiblities
+AA = ['AA', 'TT']
+AT = ['AT']
+CA = ['CA', 'AC']
+CG = ['CG']
+CT = ['CT', 'TC']
+GA = ['GA', 'AG']
+GC = ['GC']
+GT = ['GT', 'TG']
+GG = ['GG', 'CC']
+TA = ['TA']
 
-sum_NN_delta_g = AA_NN_sum + AT_NN_sum + CA_NN_sum + CG_NN_sum + CT_NN_sum + GA_NN_sum + GC_NN_sum + GT_NN_sum + GG_NN_sum + TA_NN_sum + TT_NN_sum + CC_NN_sum + AC_NN_sum + TG_NN_sum
+#Calculation of delta_G_total for each NN using data from van't Hoff script
+AA_NN_sum = AA_delta_g * sum([1.0 for i in duo_sequence if i in AA])
+AT_NN_sum = AT_delta_g * sum([1.0 for i in duo_sequence if i in AT])
+CA_NN_sum = CA_delta_g * sum([1.0 for i in duo_sequence if i in CA])
+CG_NN_sum = CG_delta_g * sum([1.0 for i in duo_sequence if i in CG])
+CT_NN_sum = CT_delta_g * sum([1.0 for i in duo_sequence if i in CT])
+GA_NN_sum = GA_delta_g * sum([1.0 for i in duo_sequence if i in GA])
+GC_NN_sum = GC_delta_g * sum([1.0 for i in duo_sequence if i in GC])
+GT_NN_sum = GT_delta_g * sum([1.0 for i in duo_sequence if i in GT]) 
+GG_NN_sum = GG_delta_g * sum([1.0 for i in duo_sequence if i in GG])
+TA_NN_sum = TA_delta_g * sum([1.0 for i in duo_sequence if i in TA])
+
+#Sum of all NN Gibb's free energy values for the input sequence
+sum_NN_delta_g = round(AA_NN_sum + AT_NN_sum + CA_NN_sum + CG_NN_sum + CT_NN_sum + GA_NN_sum + GC_NN_sum + GT_NN_sum + GG_NN_sum + TA_NN_sum, 2)
 terminal_base = sequence[-1]
 initial_base = sequence[0]
 
+#Compensation for differences in free energies bewteen the inital bases and terminal bases
 def g_total(sum_NN_delta_g):
     if terminal_base == 'G' or terminal_base == 'C' and initial_base == 'G' or initial_base == 'C':
         delta_g_total = sum_NN_delta_g + (.98)*2
@@ -71,5 +82,37 @@ def g_total(sum_NN_delta_g):
         delta_g_total = sum_NN_delta_g + 1.03 + .98
         return delta_g_total
 
+#Calculation of delta_H_total for each NN using data from van't Hoff script
+AA_h_sum = AA_delta_h * sum([1.0 for i in duo_sequence if i in AA])
+AT_h_sum = AT_delta_h * sum([1.0 for i in duo_sequence if i in AT])
+CA_h_sum = CA_delta_h * sum([1.0 for i in duo_sequence if i in CA])
+CG_h_sum = CG_delta_h * sum([1.0 for i in duo_sequence if i in CG])
+CT_h_sum = CT_delta_h * sum([1.0 for i in duo_sequence if i in CT])
+GA_h_sum = GA_delta_h * sum([1.0 for i in duo_sequence if i in GA])
+GC_h_sum = GC_delta_h * sum([1.0 for i in duo_sequence if i in GC])
+GT_h_sum = GT_delta_h * sum([1.0 for i in duo_sequence if i in GT]) 
+GG_h_sum = GG_delta_h * sum([1.0 for i in duo_sequence if i in GG])
+TA_h_sum = TA_delta_h * sum([1.0 for i in duo_sequence if i in TA])
+
+#Sum of all NN enthalpy values for the input sequence
+sum_NN_delta_h = round(AA_h_sum + AT_h_sum + CA_h_sum + CG_h_sum + CT_h_sum + GA_h_sum + GC_h_sum + GT_h_sum + GG_h_sum + TA_h_sum, 2)
+
+#Calculation of delta_S_total for each NN using data from van't Hoff script
+AA_s_sum = AA_delta_s * sum([1.0 for i in duo_sequence if i in AA])
+AT_s_sum = AT_delta_s * sum([1.0 for i in duo_sequence if i in AT])
+CA_s_sum = CA_delta_s * sum([1.0 for i in duo_sequence if i in CA])
+CG_s_sum = CG_delta_s * sum([1.0 for i in duo_sequence if i in CG])
+CT_s_sum = CT_delta_s * sum([1.0 for i in duo_sequence if i in CT])
+GA_s_sum = GA_delta_s * sum([1.0 for i in duo_sequence if i in GA])
+GC_s_sum = GC_delta_s * sum([1.0 for i in duo_sequence if i in GC])
+GT_s_sum = GT_delta_s * sum([1.0 for i in duo_sequence if i in GT]) 
+GG_s_sum = GG_delta_s * sum([1.0 for i in duo_sequence if i in GG])
+TA_s_sum = TA_delta_s * sum([1.0 for i in duo_sequence if i in TA])
+
+#Sum of all NN entropy values for the input sequence
+sum_NN_delta_s = round(AA_s_sum + AT_s_sum + CA_s_sum + CG_s_sum + CT_s_sum + GA_s_sum + GC_s_sum + GT_s_sum + GG_s_sum + TA_s_sum, 2)
+
 user_input_gc(obtainfasta)
-print(g_total(sum_NN_delta_g))
+print('The total calculated delta G total is ' + str(g_total(sum_NN_delta_g)))
+print('The total calculated delta H total is ' + str(sum_NN_delta_h))
+print('The total calculated delta S total is ' + str(sum_NN_delta_s))

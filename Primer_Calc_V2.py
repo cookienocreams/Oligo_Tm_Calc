098532d0241ab49f63cc34761ddf574d9994af8b
cookienocreams <58923195+cookienocreams@users.noreply.github.com>
2020-03-09 04:52:34 -0500

    Added more detailed comments

--- Primer_Calc_V2.py
+++ Primer_Calc_V2.py
@@ -1,20 +1,23 @@
-import math
+import math,re
 
 ###################################################################################################################################
 #Prompts to get the information required to begin the analysis
-#Primer information is input here
+#Primer and buffer information is input here
 #The primer sequences will be analyzed first by calculating their GC content
 #Analysis of primer melting temperatures occurs last
 ###################################################################################################################################
 
 primer1 = input('Input primer 1 sequence here: ') #Right now there are two inputs for non-complentary primers
 primer2 = input('Input primer 2 sequence here: ') #Will either change to a single input or figure out how to expand to batch primers
-oligo1_conc = float(input('Input total single strand oligo concentration in uM here: '))
+oligo1_conc = float(input('Input single strand oligo concentration in uM here: '))
 Mono = float(input('Input total monovalent ion concentration in mM here: '))
 Mg = float(input('Input Mg concentration in mM here: '))
 dNTPs = float(input('Input dNTP concentration in mM here: '))
 
 gas_constant = 8.31445984848484848484 #J K-1-mol-1 
+oligo_c = (oligo1_conc) * 1e-6 #Adjust concentration to mol/L
+purine = ['A','G'] #Lists used to convert primer sequence into R/Y purine/pyrimidine nucleotide codes
+pyrimidine = ['C','T']
 
 primer1_length = len(primer1)
 primer2_length = len(primer2)
@@ -25,8 +28,6 @@ p1_terminal_base = primer1[-1]
 
 p2_initial_base = primer2[0]
 p2_terminal_base = primer2[-1]
-
-oligo_c = (oligo1_conc) * 1e-6
 
 #Calculation of each primer's GC content
 def get_primer_gc(primer1, primer2):
@@ -41,60 +42,56 @@ primer1_gc, primer2_gc = get_primer_gc(primer1, primer2)
 ###################################################################################################################################
 #Determines terminal base compensation parameters for each primer
 ###################################################################################################################################
-
+'''
+For relatively short DNA sequences, such as PCR primers, the initial melting of the helix is influenced by the bases at the ends.
+DNA sequences around this length follow appoximately two-state melting. Therefore, the initial 'unzipping' plays a larger factor
+in helix melting. Regardless of the underlying mechanism, G or C bases should require more energy to begin unzipping.
+'''
 def p1_end_comp(p1_initial_base, p1_terminal_base):
 
-    AT_h, AT_s, GC_h, GC_s = 2.3, 4.1, .1, -2.8 #h in kcal/mol, s in eu
-    dh_comp, ds_comp = 0, 0
+    AT_h, AT_s, GC_h, GC_s = 2.3, 4.1, .1, -2.8 #Compensation values for enthalpy and entropy in kcal/mol and eu
+    dh_compensation, ds_compensation = 0, 0
 
     if p1_initial_base in ['A', 'T']:
-
-            dh_comp += AT_h
-            ds_comp += AT_s
+            dh_compensation += AT_h
+            ds_compensation += AT_s
 
     if p1_initial_base in ['C', 'G']:
-
-            dh_comp += GC_h
-            ds_comp += GC_s
+            dh_compensation += GC_h
+            ds_compensation += GC_s
 
     if p1_terminal_base in ['A', 'T']:
-
-            dh_comp += AT_h
-            ds_comp += AT_s
+            dh_compensation += AT_h
+            ds_compensation += AT_s
 
     if p1_terminal_base in ['C', 'G']:
+            dh_compensation += GC_h
+            ds_compensation += GC_s
 
-            dh_comp += GC_h
-            ds_comp += GC_s
-
-    return dh_comp, ds_comp
+    return dh_compensation, ds_compensation
 
 def p2_end_comp(p2_initial_base,p2_terminal_base):
 
-    AT_h, AT_s, GC_h, GC_s = 2.3, 4.1, .1, -2.8 #h in kcal/mol, s in eu
-    dh_comp, ds_comp = 0, 0
+    AT_h, AT_s, GC_h, GC_s = 2.3, 4.1, .1, -2.8 #dh in kcal/mol, ds in eu
+    dh_compensation, ds_compensation = 0, 0
 
     if p2_initial_base in ['A', 'T']:
-
-            dh_comp += AT_h
-            ds_comp += AT_s
+            dh_compensation += AT_h
+            ds_compensation += AT_s
 
     if p2_initial_base in ['C', 'G']:
-
-            dh_comp += GC_h
-            ds_comp += GC_s
+            dh_compensation += GC_h
+            ds_compensation += GC_s
 
     if p2_terminal_base in ['A', 'T']:
-
-            dh_comp += AT_h
-            ds_comp += AT_s
+            dh_compensation += AT_h
+            ds_compensation += AT_s
 
     if p2_terminal_base in ['C', 'G']:
+            dh_compensation += GC_h
+            ds_compensation += GC_s
 
-            dh_comp += GC_h
-            ds_comp += GC_s
-
-    return dh_comp, ds_comp
+    return dh_compensation, ds_compensation
 
 p1_term_h, p1_term_s = p1_end_comp(p1_initial_base,p1_terminal_base)
 p2_term_h, p2_term_s = p2_end_comp(p2_initial_base,p2_terminal_base)
@@ -105,17 +102,19 @@ p2_term_h, p2_term_s = p2_end_comp(p2_initial_base,p2_terminal_base)
 
 #Determines the primers' melting temperature based on the method developed by Privalov and Crane-Robinson
 #See Privalov, P. L., & Crane-Robinson, C. (2018). https://doi.org/10.1016/j.pbiomolbio.2018.01.007
+#Separated A and T bases to give a little more flexibility. My unfounded guess is that there is some difference
+#in water binding by adenine and thymine. Not sure if it actually helps or not.
 heat_capacity = .13 #kJ/K,mol-bp
-H_A_25, H_T_25 = 25, 24.75 #kJ/mol-bp
-S_A_25, S_T_25 = 72, 71.55 #J/K-mol-bp
+H_A_25, H_T_25 = 25, 25 #kJ/mol-bp
+S_A_25, S_T_25 = 72, 72 #J/K-mol-bp
 H_CG_25, S_CG_25 = 18.9, 44.7
 delta_S_trans = gas_constant * math.log(2 / oligo_c)
 
 def p1_melting_calculation(primer1, p1_term_h, p1_term_s):
     
-    n_A, n_T = primer1.count('A'), primer1.count('T')
+    n_A, n_T = primer1.count('A'), primer1.count('T') #Counts the number of each base in the primer
     n_CG = primer1.count('C') + primer1.count('G')
-
+    #Total enthalpy and entropy value calculations for determining the melting temperature. Takes the heat capacity into account.
     delta_H = ((H_A_25 + (heat_capacity * (311.5 - 298.15))) * n_A) + ((H_T_25 + (heat_capacity * (311.5 - 298.15))) * n_T) + ((H_CG_25 + (heat_capacity * (311.5 - 298.15))) * n_CG) + p1_term_h
     delta_S = ((S_A_25 + (heat_capacity * math.log(311.5 / 298.15))) * n_A) + ((S_T_25 + (heat_capacity * math.log(311.5 / 298.15))) * n_T) + ((S_CG_25 + (heat_capacity * math.log((311.5) / 298.15))) * n_CG) + p1_term_s
 
@@ -150,8 +149,14 @@ mg = (-(ka * dntps - ka * mg_adj + 1.0) + math.sqrt((ka * dntps - ka * mg_adj + 
 #See Owczarzy, R., et al. (2008). Biochemistry, https://doi.org/10.1021/bi702363u
 def primer1_salt_correction(primer1_melting_temperature, primer1_gc, primer1_length, mg, mon):
 
-    a, b, c, d, e, f, g = 3.92e-5, -9.11e-6, 6.26e-5, 1.42e-5, -4.82e-4, 5.25e-4, 8.31e-5
-    
+    a, b, c, d, e, f, g = 3.92e-5, -9.11e-6, 6.06e-5, 1.42e-5, -4.92e-4, 5.35e-4, 8.31e-5
+    '''
+The condition calculating the melting temperature if there is no monovalent salt present must be put first, before calculating R.
+This is because the R equation requires dividing by the monovalent ion concentration. And of course, you can't divide by zero.
+Additionally, although the condition of R > 6 uses the same equation as when no monovalent ions are present, the two conditions
+can't be combined. In situations where no magnesium is present, the equation would try to take the natural log of zero, which
+throws an error.
+'''
     if mon == 0:
 
         salt2 = (1 / (primer1_melting_temperature + 273.15)) + a + (b * math.log(mg)) + ((primer1_gc / 100) * (c + d * math.log(mg))) + (1 / (2.0 * (primer1_length - 1))) * (e + f * math.log(mg) + g * math.log(mg) ** 2)
@@ -184,7 +189,7 @@ def primer1_salt_correction(primer1_melting_temperature, primer1_gc, primer1_len
 
 def primer2_salt_correction(primer2_melting_temperature, primer2_gc, primer2_length, mg, mon):
 
-    a, b, c, d, e, f, g = 3.92e-5, -9.11e-6, 6.26e-5, 1.42e-5, -4.82e-4, 5.25e-4, 8.31e-5
+    a, b, c, d, e, f, g = 3.92e-5, -9.11e-6, 6.06e-5, 1.42e-5, -4.92e-4, 5.35e-4, 8.31e-5
     
     if mon == 0:
 
@@ -222,23 +227,32 @@ adj_primer2_melting_temperature = primer2_salt_correction(primer2_melting_temper
 #Additional melting temperature adjustment to account for the fact that Privalov and Crane-Robinson's model accuracy can
 #vary significantly depending on the GC content of the oligo. It tends to underestimate the Tm of low GC oligos and overestimates
 #the Tm of high GC oligos. The highest accuracy is at a GC% around 50. Oligo length is also taken into account here.
-def gc_tm_adjustment(adj_primer1_melting_temperature, adj_primer2_melting_temperature, primer1_gc, primer2_gc):
-    
-    low_gc, mid_gc, high_gc = .166667, .376667, .466667 #Correction factors for three GC% tiers
+
+def p1_gc_tm_adjustment(adj_primer1_melting_temperature, primer1_gc):
+
+    '''
+Correction factors for three GC% tiers. The correction values were calculated using the difference between the predicted melting
+temperature using the standard model and the measured melting temperature from the 92 oligos in Owczarzy's 2004 paper. See
+Owczarzy, R. et. al (2004). Biochemistry. https://doi.org/10.1021/bi034621r The differences were from around -6C for 20% GC 
+to 14C for 80% GC. The melting temperature difference was then divided by the difference between the GC% where the model was most 
+accurate, ~50%, and the GC% of the oligos at 20% and 80% GC. Since the melting temperature difference increased with oligo length, 
+higher correction values are used to account for that length dependent effect.
+'''
+    low_gc, mid_gc, high_gc = .166667, .366667, .466667
 
     if primer1_gc < 45:
-
+        #Multiplies the correction factor by the GC% difference between the primer and the approximate 'highest accuracy' GC%.
         p1_tm = float(adj_primer1_melting_temperature + (low_gc * (52 - primer1_gc)))
 
-    elif primer1_gc >= 45 and primer1_gc <= 65:
-        if primer1_length >=25 and round(primer1_gc) in [47,48,49,50,51,52]:
+    elif primer1_gc >= 45 and primer1_gc < 60:
+        if primer1_length >=25 and round(primer1_gc) in [47,48,49,50,51,52]:#The values here are arbitrary and are subject to change
 
             p1_tm = float(.959 * (adj_primer1_melting_temperature - (mid_gc * (primer1_gc - 50))))
         else:
             p1_tm = float((adj_primer1_melting_temperature - (mid_gc * (primer1_gc - 50))))
 
     else:
-        if primer1_length <= 15:
+        if primer1_length <= 15: #Length cut-offs were based on the 92 oligos but are subject to change
 
             high_gc = .266667
             p1_tm = float(adj_primer1_melting_temperature - (high_gc * (primer1_gc - 48))) 
@@ -249,13 +263,17 @@ def gc_tm_adjustment(adj_primer1_melting_temperature, adj_primer2_melting_temper
 
             high_gc = .566667
             p1_tm = float(adj_primer1_melting_temperature - (high_gc * (primer1_gc - 48))) 
-        return p1_tm
+    return p1_tm
+
+def p2_gc_tm_adjustment(adj_primer2_melting_temperature, primer2_gc):
+
+    low_gc, mid_gc, high_gc = .166667, .366667, .466667
 
     if primer2_gc < 45:
 
         p2_tm = float(adj_primer2_melting_temperature + (low_gc * (52 - primer2_gc)))
 
-    elif primer2_gc >= 45 and primer2_gc <= 65:
+    elif primer2_gc >= 45 and primer2_gc < 60:
         if primer2_length >=25 and round(primer2_gc) in [47,48,49,50,51,52]:
 
             p2_tm = float(.959 * (adj_primer2_melting_temperature - (mid_gc * (primer2_gc - 50))))
@@ -277,15 +295,15 @@ def gc_tm_adjustment(adj_primer1_melting_temperature, adj_primer2_melting_temper
             high_gc = .566667
             p2_tm = float(adj_primer2_melting_temperature - (high_gc * (primer2_gc - 48))) 
 
-    return p1_tm, p2_tm
+    return p2_tm
 
-final_tm_p1, final_tm_p2 = gc_tm_adjustment(adj_primer1_melting_temperature, adj_primer2_melting_temperature, primer1_gc, primer2_gc)
+p1_tm = p1_gc_tm_adjustment(adj_primer1_melting_temperature, primer1_gc)
+p2_tm = p2_gc_tm_adjustment(adj_primer2_melting_temperature, primer2_gc)
 
 ###################################################################################################################################
 #Printing the results
 ###################################################################################################################################
-
 print('The GC content of primer 1 is ' + str(round(primer1_gc, 1)) + '%.' + 
 ' The GC content of primer 2 is ' + str(round(primer2_gc, 1)) + '%.')
-print('The melting temperatures of your primers are ' + str(round(final_tm_p1, 1)) + 
-'C for primer 1, and ' + str(round(final_tm_p2, 1)) + 'C for primer 2.')
+print('The melting temperatures of your primers are ' + str(round(p1_tm, 1)) + 
+'C for primer 1, and ' + str(round(p2_tm, 1)) + 'C for primer 2.')

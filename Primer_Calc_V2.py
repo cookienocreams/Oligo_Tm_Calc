b6d5eee93c8c7a1fee8072167632b951f1e88d7f
cookienocreams <58923195+cookienocreams@users.noreply.github.com>
2020-03-26 19:03:42 -0500

    Length dependent compensation + spelling fixes

--- Primer_Calc_V2.py
+++ Primer_Calc_V2.py
@@ -35,7 +35,7 @@ different base sequences but the same GC content. Those types of oligos can diff
 Privalov and Crane-Robinson's model doesn't contain any way to account for those differences. I think the discrepancy 
 may be due to the presence of alternating purine and pyrimidine bases. The alternating pairs may break up water binding
 to adenine and thymine residues, thus decreasing the entropy cost associated with that water binding. In theory, the longer 
-the alternating purine and pyrimidine chain, the more stable the helix is. Conversely, the more long strings of repeating
+the alternating purine and pyrimidine chain, the more stable the helix is. Conversely, the longer the strings of repeating
 pyrimidine bases, the less stable the helix.
 '''
 AT_pairs_p1 = re.findall('(?:A|T){1}(?:A|T){1,}', primer1) #Generates a list of repeating adenine and thymine sequences
@@ -53,9 +53,8 @@ RY_pairs_p2 = re.findall('(?:RY){2,}', RY_primer2_str) #in each primer. Only inc
 YY_pairs_p1 = re.findall('(?:YY){2,}', RY_primer1_str) #Generates a list of repeating pyrimidine sequences
 YY_pairs_p2 = re.findall('(?:YY){2,}', RY_primer2_str) ##in each primer. Only includes sequences with two or more repeats.
 '''
-The above lists are needed so that their respective influences on helix stability can be measured and quantified, assuming
-there is some influence. Don't yet know how best to do that. The functions below attempts to apply those differences to appropriately 
-modify the melting temperature.
+The above lists are needed so that their respective influences on helix stability can be measured and quantified. The functions 
+below attempt to apply those differences to appropriately modify the melting temperature.
 '''
 #These variables take those captured sequences from the lists of pairs above and convert them into lengths. The lengths will then
 #be used to alter the entropy values during the melting temperature calculation.
@@ -68,7 +67,7 @@ p2_AT_len = [len(seq) for seq in AT_pairs_p2]
 p2_GC_len = [len(seq) for seq in GC_pairs_p2]
 p2_RY_len = [len(seq) for seq in RY_pairs_p2]
 p2_YY_len = [len(seq) for seq in YY_pairs_p2]
-#Here the melting temperature of the primers is determeined based on the method developed by Privalov and Crane-Robinson
+#Here the melting temperature of the primers is determined based on the method developed by Privalov and Crane-Robinson
 #See Privalov, P. L., & Crane-Robinson, C. (2018). https://doi.org/10.1016/j.pbiomolbio.2018.01.007
 heat_capacity = .13 #kJ/K,mol-bp
 H_A_25, H_T_25 = 25, 25 #kJ/mol-bp
@@ -84,14 +83,16 @@ def p1_melting_calculation(primer1):
     #Total enthalpy and entropy value calculations for determining the melting temperature. Takes the heat capacity into account.
     delta_H = ((H_A_25 + (heat_capacity * (314.5 - 298.15))) * n_A) + ((H_T_25 + (heat_capacity * (314.5 - 298.15))) * n_T) + ((H_CG_25 + (heat_capacity * (314.5 - 298.15))) * n_CG)
     delta_S = ((S_A_25 + (heat_capacity * math.log(314.5 / 298.15))) * n_A) + ((S_T_25 + (heat_capacity * math.log(314.5 / 298.15))) * n_T) + ((S_CG_25 + (heat_capacity * math.log((314.5) / 298.15))) * n_CG)
-    if primer1_length > 15:
-        delta_S -= (-.0251 * (primer1_gc) ** 2 + .9798 * (primer1_gc) + 11.852)
-    else:
-        delta_S -= (-.0064 * (primer1_gc) ** 2 - .0647 * (primer1_gc) + 23.153)
     '''
-Corrections for destabilzing synergystic effects of water chaining and the stabilizing effects of disrupting that chaining. While RY/YY
-combos are imperfect, as an RY pair may be ATA, which could form water chains and vice versa for YY pairs, adding them both should roughly
-cancel out over/underestimations of their stabilization effects.
+The equation below functions as an approximate "best fit" correction for the discrepancy in the model's accuracy at high and low %GC. It 
+adjust the constants of a polynomial equation based on chnages in oligo length. The values here were derived from the melting temperatures
+from the 92 oligos in Owczarzy's 2004 paper. See Owczarzy, R. et. al (2004). Biochemistry. https://doi.org/10.1021/bi034621r.
+'''   
+    delta_S -= ((-.0013 * (primer2_length) + .0086) * (primer2_gc) ** 2 + (.0601 * (primer2_length) - .6562) * (primer2_gc) + (-.0481 * (primer2_length) ** 2 + 1.3841 * (primer2_length) + 10.826))
+    '''
+Additional corrections for destabilizing synergistic effects of water chaining and the stabilizing effects of disrupting that chaining. 
+While RY/YY combos are imperfect, as an RY pair may be ATA, which could form water chains and vice versa for YY pairs, adding them both 
+should roughly cancel out over/underestimations of their stabilization effects.
 '''    
     for num in p1_AT_len:
         if num >= 2:
@@ -115,10 +116,8 @@ def p2_melting_calculation(primer2):
 
     delta_H = ((H_A_25 + (heat_capacity * (314.5 - 298.15))) * n_A) + ((H_T_25 + (heat_capacity * (314.5 - 298.15))) * n_T) + ((H_CG_25 + (heat_capacity * (314.5 - 298.15))) * n_CG)
     delta_S = ((S_A_25 + (heat_capacity * math.log(314.5 / 298.15))) * n_A) + ((S_T_25 + (heat_capacity * math.log(314.5 / 298.15))) * n_T) + ((S_CG_25 + (heat_capacity * math.log((314.5) / 298.15))) * n_CG)
-    if primer2_length > 15:
-        delta_S -= (-.0251 * (primer2_gc) ** 2 + .9798 * (primer2_gc) + 11.852)
-    else:
-        delta_S -= (-.0064 * (primer2_gc) ** 2 - .0647 * (primer2_gc) + 23.153)
+    
+    delta_S -= ((-.0013 * (primer2_length) + .0086) * (primer2_gc) ** 2 + (.0601 * (primer2_length) - .6562) * (primer2_gc) + (-.0481 * (primer2_length) ** 2 + 1.3841 * (primer2_length) + 10.826))
 
     for num in p2_AT_len:
         if num >= 2:
@@ -141,7 +140,6 @@ primer2_melting_temperature = p2_melting_calculation(primer2)
 ###################################################################################################################################
 #Salt and Magnesium Adjustments
 ###################################################################################################################################
-
 #Adjustments and unit conversions for the chosen buffer conditions
 Mon = Mono / 2.0 #Divide by two to account for the counterion present, e.g. Cl-, SO4-, etc.
 mg_adj = Mg * 1e-3 #Converts to mol/L
@@ -153,7 +151,7 @@ mg = (-(ka * dntps - ka * mg_adj + 1.0) + math.sqrt((ka * dntps - ka * mg_adj + 
 #Equations from Owczarzy 2008 that adjusts melting temperatures according to monovalent ion, magnesium, and dNTP concentrations
 #See Owczarzy, R., et al. (2008). Biochemistry, https://doi.org/10.1021/bi702363u
 def primer1_salt_correction(primer1_melting_temperature, primer1_gc, primer1_length, mg, mon):
-    a, b, c, d, e, f, g = 3.92e-5, -9.11e-6, 6.66e-5, 1.42e-5, -4.02e-4, 4.05e-4, 8.31e-5 #Slightly modified constants
+    a, b, c, d, e, f, g = 3.92e-5, -10.11e-6, 7.76e-5, 1.42e-5, -3.02e-4, 3.30e-4, 8.31e-5 #Slightly modified constants
     '''
 The condition calculating the melting temperature if there is no monovalent salt present must be put first, before calculating R.
 This is because the R equation requires dividing by the monovalent ion concentration. And of course, you can't divide by zero.
@@ -179,10 +177,10 @@ throws an error.
         return round((1 / salt2) - 273.15, 1)
     elif R > 6.0:
         salt2 = (1 / (primer1_melting_temperature + 273.15)) + a + (b * math.log(mg)) + ((primer1_gc / 100) * (c + d * math.log(mg))) + (1 / (2.0 * (primer1_length - 1))) * (e + f * math.log(mg) + g * math.log(mg) ** 2)
-    return round((1 / salt2) - 273.15,1)
+    return round((1 / salt2) - 273.15, 1)
 
 def primer2_salt_correction(primer2_melting_temperature, primer2_gc, primer2_length, mg, mon):
-    a, b, c, d, e, f, g = 3.92e-5, -9.11e-6, 6.66e-5, 1.42e-5, -4.02e-4, 4.05e-4, 8.31e-5
+    a, b, c, d, e, f, g = 3.92e-5, -10.11e-6, 7.76e-5, 1.42e-5, -3.02e-4, 3.30e-4, 8.31e-5
     if mon == 0:
         salt2 = (1 / (primer2_melting_temperature + 273.15)) + a + (b * math.log(mg)) + ((primer2_gc / 100) * (c + d * math.log(mg))) + (1 / (2.0 * (primer2_length - 1))) * (e + f * math.log(mg) + g * math.log(mg) ** 2)
         return round((1 / salt2) - 273.15, 1)
@@ -201,14 +199,12 @@ def primer2_salt_correction(primer2_melting_temperature, primer2_gc, primer2_len
         return round((1 / salt2) - 273.15, 1)
     elif R > 6.0:
         salt2 = (1 / (primer2_melting_temperature + 273.15)) + a + (b * math.log(mg)) + ((primer2_gc / 100) * (c + d * math.log(mg))) + (1 / (2.0 * (primer2_length - 1))) * (e + f * math.log(mg) + g * math.log(mg) ** 2)
-        return round((1 / salt2) - 273.15,1)
+        return round((1 / salt2) - 273.15, 1)
 
 adj_primer1_melting_temperature = primer1_salt_correction(primer1_melting_temperature, primer1_gc, primer1_length, mg, mon)
 adj_primer2_melting_temperature = primer2_salt_correction(primer2_melting_temperature, primer2_gc, primer2_length, mg, mon)
 ###################################################################################################################################
 #Printing the results
 ###################################################################################################################################
-print('The GC content of primer 1 is ' + str(round(primer1_gc, 1)) + '%.' + 
-' The GC content of primer 2 is ' + str(round(primer2_gc, 1)) + '%.')
-print('The melting temperatures of your primers are ' + str(adj_primer1_melting_temperature) + 
-'C for primer 1, and ' + str(adj_primer2_melting_temperature) + 'C for primer 2.')
+print('The GC content of primer 1 is ' + str(round(primer1_gc, 1)) + '%.' + ' The GC content of primer 2 is ' + str(round(primer2_gc, 1)) + '%.')
+print('The melting temperatures of your primers are ' + str(adj_primer1_melting_temperature) + 'C for primer 1 and ' + str(adj_primer2_melting_temperature) + 'C for primer 2.')

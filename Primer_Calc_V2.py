7c3d37f062b2bd18e9ed5271188737ca71509d36
cookienocreams <58923195+cookienocreams@users.noreply.github.com>
2020-04-03 16:38:00 -0500

    Updated correction equation + added Term comp

--- Primer_Calc_V2.py
+++ Primer_Calc_V2.py
@@ -18,6 +18,8 @@ oligo_c = (oligo1_conc) * 1e-6 #Adjust concentration to mol/L
 purine = ['A','G'] #Lists used to convert primer sequence into R/Y purine/pyrimidine nucleotide codes
 pyrimidine = ['C','T']
 primer1_length, primer2_length = len(primer1), len(primer2)
+p1_terminal_base, p1_initial_base = primer1[-1], primer1[0]
+p2_terminal_base, p2_initial_base = primer2[-1], primer2[0]
 
 #Calculation of each primer's GC content
 def get_primer_gc(primer1, primer2):
@@ -26,6 +28,50 @@ def get_primer_gc(primer1, primer2):
     return primer1_GC, primer2_GC
 
 primer1_gc, primer2_gc = get_primer_gc(primer1, primer2)
+###################################################################################################################################
+#Determine terminal base compensation parameters for each primer
+###################################################################################################################################
+'''
+For relatively short DNA sequences, such as PCR primers, the initial melting of the helix is influenced by the bases at the ends.
+DNA sequences around this length follow appoximately two-state melting. Therefore, the initial 'unzipping' plays a larger factor
+in helix melting. Regardless of the underlying mechanism, G or C bases should require more energy to begin unzipping.
+'''
+def p1_end_comp(p1_initial_base, p1_terminal_base):
+    AT_h, AT_s, GC_h, GC_s = 2.53, 5.7, .49, -.7
+    dh_comp, ds_comp = 0, 0
+    if p1_initial_base in ['A', 'T']:
+        dh_comp += AT_h
+        ds_comp += AT_s
+    if p1_initial_base in ['C', 'G']:
+        dh_comp += GC_h
+        ds_comp += GC_s
+    if p1_terminal_base in ['A', 'T']:
+        dh_comp += AT_h
+        ds_comp += AT_s
+    if p1_terminal_base in ['C', 'G']:
+        dh_comp += GC_h
+        ds_comp += GC_s
+    return dh_comp, ds_comp
+
+def p2_end_comp(p2_initial_base,p2_terminal_base):
+    AT_h, AT_s, GC_h, GC_s = 2.53, 5.7, .49, -.7
+    dh_comp, ds_comp = 0, 0
+    if p2_initial_base in ['A', 'T']:
+        dh_comp += AT_h
+        ds_comp += AT_s
+    if p2_initial_base in ['C', 'G']:
+        dh_comp += GC_h
+        ds_comp += GC_s
+    if p2_terminal_base in ['A', 'T']:
+        dh_comp += AT_h
+        ds_comp += AT_s
+    if p2_terminal_base in ['C', 'G']:
+        dh_comp += GC_h
+        ds_comp += GC_s
+    return dh_comp, ds_comp
+
+p1_term_h, p1_term_s = p1_end_comp(p1_initial_base,p1_terminal_base)
+p2_term_h, p2_term_s = p2_end_comp(p2_initial_base,p2_terminal_base)
 ###################################################################################################################################
 #Melting Temperature Calculation
 ###################################################################################################################################
@@ -47,8 +93,8 @@ RY_primer2_str = ''.join(['R' if base in purine else 'Y' for base in primer2]) #
 RY_pairs_p1 = re.findall('(?:RY){1,}', RY_primer1_str) #Generates a list of repeating purine and pyrimidine sequences
 RY_pairs_p2 = re.findall('(?:RY){1,}', RY_primer2_str) #in each primer. Only includes sequences with two or more repeats.
 
-YY_pairs_p1 = re.findall('(?:Y){2,}', RY_primer1_str) + re.findall('(?:R){2,}', RY_primer1_str) #Generates a list of repeating purine
-YY_pairs_p2 = re.findall('(?:Y){2,}', RY_primer2_str) + re.findall('(?:R){2,}', RY_primer2_str) #sequences in each primer.
+YY_pairs_p1 = re.findall('(?:Y){2,}', RY_primer1_str) + re.findall('(?:R){2,}', RY_primer1_str) #Generates a list of repeating purine and
+YY_pairs_p2 = re.findall('(?:Y){2,}', RY_primer2_str) + re.findall('(?:R){2,}', RY_primer2_str) #pyrimidine sequences in each primer.
 '''
 The above lists are needed so that their respective influences on helix stability can be measured and quantified. The functions 
 below attempts to apply those differences to appropriately modify the melting temperature.
@@ -66,9 +112,8 @@ p2_YY_len = [len(seq) for seq in YY_pairs_p2]
 #See Privalov, P. L., & Crane-Robinson, C. (2018). https://doi.org/10.1016/j.pbiomolbio.2018.01.007
 heat_capacity = .13 #kJ/K,mol-bp
 H_A_25, H_T_25 = 25, 25 #kJ/mol-bp
-S_A_25, S_T_25 = 71.5, 72 #J/K-mol-bp
+S_A_25, S_T_25 = 72, 72 #J/K-mol-bp
 H_CG_25, S_CG_25 = 18.8, 44.7
-delta_S_trans = gas_constant * math.log(2 / oligo_c)
 
 def p1_melting_calculation(primer1):
 
@@ -76,48 +121,50 @@ def p1_melting_calculation(primer1):
     n_CG = primer1.count('C') + primer1.count('G') #Needed to calculate total enthalpy and entropy
     
     #Total enthalpy and entropy value calculations for determining the melting temperature. Takes the heat capacity into account.
-    delta_H = ((H_A_25 + (heat_capacity * (314.5 - 298.15))) * n_A) + ((H_T_25 + (heat_capacity * (314.5 - 298.15))) * n_T) + ((H_CG_25 + (heat_capacity * (314.5 - 298.15))) * n_CG)
-    delta_S = ((S_A_25 + (heat_capacity * math.log(314.5 / 298.15))) * n_A) + ((S_T_25 + (heat_capacity * math.log(314.5 / 298.15))) * n_T) + ((S_CG_25 + (heat_capacity * math.log((314.5) / 298.15))) * n_CG)
+    delta_H = ((H_A_25 + (heat_capacity * (314.5 - 298.15))) * n_A) + ((H_T_25 + (heat_capacity * (314.5 - 298.15))) * n_T) + ((H_CG_25 + (heat_capacity * (314.5 - 298.15))) * n_CG) + p1_term_h
+    delta_S = ((S_A_25 + (heat_capacity * math.log(314.5 / 298.15))) * n_A) + ((S_T_25 + (heat_capacity * math.log(314.5 / 298.15))) * n_T) + ((S_CG_25 + (heat_capacity * math.log((314.5) / 298.15))) * n_CG) + p1_term_s
     '''
 The equation below functions as an approximate "best fit" correction for the discrepancy in the model's accuracy at high and low %GC. It 
 adjust the constants of a polynomial equation based on chnages in oligo length. The values here were derived from the melting temperatures
 from the 92 oligos in Owczarzy's 2004 paper. See Owczarzy, R. et. al (2004). Biochemistry. https://doi.org/10.1021/bi034621r.
 '''   
-    delta_S -= ((-.0013 * (primer1_length) + .0086) * (primer1_gc) ** 2 + (.0601 * (primer1_length) - .6562) * (primer1_gc) + \
-        (-.0481 * (primer1_length) ** 2 + 1.3841 * (primer1_length) + 10.826))
+    delta_S -= ((-.001292 * (primer1_length) + .00988) * (primer1_gc) ** 2 + (.0572 * (primer1_length) - .7342) * (primer1_gc) + \
+        (-.0366 * (primer1_length) ** 2 + .839 * (primer1_length) + 23.863))
     '''
 Additional corrections for the combination of site specific DNA bending, ion binding to specific sequences, plus other assorted solvent
 effects which are too complex to fully account for. The following try to account for divalent ion binding to G bases which may displace
 part of the water spine therefore reducing entropy. The other YY and RY corrections try to account for the hightened twist-roll coupling
 that occurs most often in pyrimidine-purine dimers and least often in purine-pyrimidine steps.
 '''    
-    for num in p1_GC_len:
-        delta_S -= (.88 * num)
-    for num in p1_RY_len:
-        delta_S -= (.55 * num)
-    for num in p1_YY_len:
-        delta_S += (.85 * num)
-
+    if primer1_length < 25: #Corrections are most helpful for oligos shorter than around 25 bp 
+        for num in p1_GC_len:
+            delta_S -= (.70 * num)
+        for num in p1_RY_len:
+            delta_S -= (.25 * num)
+        for num in p1_YY_len:
+            delta_S += (.65 * num)
     tm = (1000 * delta_H) / (delta_S + (gas_constant * (math.log(2 / oligo_c)))) - 298.15
+
     return tm
 
 def p2_melting_calculation(primer2):
 
     n_A, n_T, n_CG = primer2.count('A'), primer2.count('T'), primer2.count('C') + primer2.count('G')
 
-    delta_H = ((H_A_25 + (heat_capacity * (314.5 - 298.15))) * n_A) + ((H_T_25 + (heat_capacity * (314.5 - 298.15))) * n_T) + ((H_CG_25 + (heat_capacity * (314.5 - 298.15))) * n_CG)
-    delta_S = ((S_A_25 + (heat_capacity * math.log(314.5 / 298.15))) * n_A) + ((S_T_25 + (heat_capacity * math.log(314.5 / 298.15))) * n_T) + ((S_CG_25 + (heat_capacity * math.log((314.5) / 298.15))) * n_CG)
+    delta_H = ((H_A_25 + (heat_capacity * (314.5 - 298.15))) * n_A) + ((H_T_25 + (heat_capacity * (314.5 - 298.15))) * n_T) + ((H_CG_25 + (heat_capacity * (314.5 - 298.15))) * n_CG) + p2_term_h
+    delta_S = ((S_A_25 + (heat_capacity * math.log(314.5 / 298.15))) * n_A) + ((S_T_25 + (heat_capacity * math.log(314.5 / 298.15))) * n_T) + ((S_CG_25 + (heat_capacity * math.log((314.5) / 298.15))) * n_CG) + p2_term_s
     
-    delta_S -= ((-.0013 * (primer2_length) + .0086) * (primer2_gc) ** 2 + (.0601 * (primer2_length) - .6562) * (primer2_gc) + (-.0481 * (primer2_length) ** 2 + 1.3841 * (primer2_length) + 10.826))
-
-    for num in p2_GC_len:
-        delta_S -= (.88 * num)
-    for num in p2_RY_len:
-        delta_S -= (.55 * num)
-    for num in p2_YY_len:
-        delta_S += (.85 * num)
-
+    delta_S -= ((-.001292 * (primer2_length) + .00988) * (primer2_gc) ** 2 + (.0572 * (primer2_length) - .7342) * (primer2_gc) + \
+        (-.0366 * (primer2_length) ** 2 + .839 * (primer2_length) + 23.863))
+    if primer2_length < 25:
+        for num in p2_GC_len:
+            delta_S -= (.70 * num)
+        for num in p2_RY_len:
+            delta_S -= (.25 * num)
+        for num in p2_YY_len:
+            delta_S += (.65 * num)
     tm = (1000 * delta_H) / (delta_S + (gas_constant * (math.log(2 / oligo_c)))) - 298.15
+
     return tm
 
 primer1_melting_temperature = p1_melting_calculation(primer1)

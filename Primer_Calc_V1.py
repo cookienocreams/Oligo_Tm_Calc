e3dbe549a0aa4990d3339f5347ac61132826e374
cookienocreams <58923195+cookienocreams@users.noreply.github.com>
2020-05-22 18:27:05 -0500

    Chi-square optimized parameter update

--- Primer_Calc_V1.py
+++ Primer_Calc_V1.py
@@ -1,49 +1,5 @@
 import re, math
 
-###################################################################################################################################
-#Modified Nearest Neighbor Parameters based on SantaLucia's 1998 paper
-###################################################################################################################################
-
-AA_delta_s = -22.15
-AA_delta_h = -7.845
-AA_delta_g = AA_delta_h - (273.15*(AA_delta_s/1000))
-
-AC_delta_s = -20.125
-AC_delta_h = -7.97
-AC_delta_g = AC_delta_h - (273.15*(AC_delta_s/1000))
-
-AG_delta_s = -21.125
-AG_delta_h = -8.335
-AG_delta_g = AG_delta_h - (273.15*(AG_delta_s/1000))
-
-AT_delta_s = -20.315
-AT_delta_h = -7.12
-AT_delta_g = AT_delta_h - (273.15*(AT_delta_s/1000))
-
-CA_delta_s = -22.27
-CA_delta_h = -8.63
-CA_delta_g = CA_delta_h - (273.15*(CA_delta_s/1000))
-
-CC_delta_s = -20.65
-CC_delta_h = -7.825
-CC_delta_g = CC_delta_h - (273.15*(CC_delta_s/1000))
-
-CG_delta_s = -24.95
-CG_delta_h = -10.02
-CG_delta_g = CG_delta_h - (273.15*(CG_delta_s/1000))
-
-GA_delta_s = -22.47
-GA_delta_h = -8.43
-GA_delta_g = GA_delta_h - (273.15*(GA_delta_s/1000))
-
-GC_delta_s = -24.7
-GC_delta_h = -9.65
-GC_delta_g = GC_delta_h - (273.15*(GC_delta_s/1000))
-
-TA_delta_s = -20.365
-TA_delta_h = -7.2
-TA_delta_g = TA_delta_h - (273.15*(TA_delta_s/1000))
-
 #Lists with all non-overlapping NN pair possiblities
 AA = ['AA', 'TT']
 AC = ['AC', 'TG']
@@ -57,7 +13,10 @@ GC = ['GC']
 TA = ['TA']
 
 gas_constant = 1.98720425864083 #cal⋅K−1⋅mol−1
-
+purine = ['A','G'] #Lists used to convert primer sequence into R/Y purine/pyrimidine nucleotide codes
+pyrimidine = ['C','T']
+#End Compensation parameters
+YY_h,RY_h = 1.180248081487909, 0.9577358235882366 #h in kcal/mol, s in eu
 ###################################################################################################################################
 #Prompts to get the information required to begin the analysis
 #Primer information is input here
@@ -72,21 +31,21 @@ Mono = float(input('Input total monovalent ion concentration in mM here: '))
 Mg = float(input('Input Mg concentration in mM here: '))
 dNTPs = float(input('Input dNTP concentration in mM here: '))
 
-#Captures the first and last base in each primer so that the energies associated with opening the helix can be calculated
-p1_initial_base = primer1[0]
-p1_terminal_base = primer1[-1]
+primer1_length = len(primer1)
+primer2_length = len(primer2)
 
-p2_initial_base = primer2[0]
-p2_terminal_base = primer2[-1]
+#Captures the first and last base in each primer so that the energies associated with opening the helix can be calculated
+p1_RY_seq_str = ''.join(['R' if base in purine else 'Y' for base in primer1])#Converts each primer into purine/pyrimidine strings
+p2_RY_seq_str = ''.join(['R' if base in purine else 'Y' for base in primer2])
+
+p1_initial_bases, p1_terminal_bases = p1_RY_seq_str[0:2], p1_RY_seq_str[-2:primer1_length]
+p2_initial_bases, p2_terminal_bases = p2_RY_seq_str[0:2], p2_RY_seq_str[-2:primer2_length]
 
 #Splits input primers into doublets for easy NN matching below
 duo_primer1 = re.findall('.{1,2}', primer1) + re.findall('.{1,2}', primer1[1:])
 duo_primer2 = re.findall('.{1,2}', primer2) + re.findall('.{1,2}', primer2[1:])
 
-primer1_length = len(primer1)
-primer2_length = len(primer2)
-
-oligo_c = (oligo1_conc / 2.0) * 1e-9
+oligo_c = (oligo1_conc) * 1e-6 #Adjust concentration to mol/L
 
 #Calculation of each primer's GC content
 def get_primer_gc(primer1, primer2):
@@ -97,7 +56,38 @@ def get_primer_gc(primer1, primer2):
     return primer1_GC, primer2_GC
 
 primer1_gc, primer2_gc = get_primer_gc(primer1, primer2)
+###################################################################################################################################
+#Chi-square Optimized Nearest Neighbor Parameters
+###################################################################################################################################
+AA_delta_s = -22.15461879548201
+AA_delta_h = -7.909076567833574
 
+AC_delta_s = -20.20944930135789
+AC_delta_h = -7.564931774075489
+
+AG_delta_s = -20.85324697548337
+AG_delta_h = -7.866141135756189
+
+AT_delta_s = -19.931105027077287
+AT_delta_h = -7.114678926718001
+
+CA_delta_s = -21.383943453258333
+CA_delta_h = -7.814334448491746
+
+CC_delta_s = -19.677205419525077
+CC_delta_h = -7.780021121897234
+
+CG_delta_s = -24.335240882791073
+CG_delta_h = -9.344024883725648
+
+GA_delta_s = -21.617116510641825
+GA_delta_h = -7.934729350061229
+
+GC_delta_s = -23.896038387305747
+GC_delta_h = -9.399037969893644
+
+TA_delta_s = -19.12903239340641
+TA_delta_h = -6.580371705207274
 ###################################################################################################################################
 #Determination of NN thermodynamic parameters for each primer
 ###################################################################################################################################
@@ -156,49 +146,38 @@ sum_p1_delta_s = AA_s_p1 + AC_s_p1 + AG_s_p1 + AT_s_p1 + CA_s_p1 + CC_s_p1 + CG_
 sum_p2_delta_h = AA_h_p2 + AC_h_p2 + AG_h_p2 + AT_h_p2 + CA_h_p2 + CC_h_p2 + CG_h_p2 + GA_h_p2 + GC_h_p2 + TA_h_p2
 sum_p2_delta_s = AA_s_p2 + AC_s_p2 + AG_s_p2 + AT_s_p2 + CA_s_p2 + CC_s_p2 + CG_s_p2 + GA_s_p2 + GC_s_p2 + TA_s_p2
 
-#Calculates the entropic and enthalpic compensation for the ends of each primer
-def p1_terminal_comp(p1_initial_base, p1_terminal_base, sum_p1_delta_h, sum_p1_delta_s):
+#Calculates the enthalpic compensation for the primer ends and adds it to the total enthalpy
+def p1_end_comp(p1_initial_bases,p1_terminal_bases,sum_p1_delta_h):
+    if p1_initial_bases in ['YY', 'RR']:
+        sum_p1_delta_h += YY_h
+    if p1_initial_bases in ['RY', 'YR']:
+        sum_p1_delta_h += RY_h
+    if p1_terminal_bases in ['YY', 'RR']:
+        sum_p1_delta_h += YY_h
+    if p1_terminal_bases in ['RY', 'YR']:
+        sum_p1_delta_h += RY_h
+    return sum_p1_delta_h
 
-    AT_h, AT_s, GC_h, GC_s = 2.4, 5.7, .45, -1.7 #h in kcal/mol, s in eu
-    dh, ds = 0, 0
+def p2_end_comp(p2_initial_bases,p2_terminal_bases,sum_p2_delta_h):
+    if p2_initial_bases in ['YY', 'RR']:
+        sum_p2_delta_h += YY_h
+    if p2_initial_bases in ['RY', 'YR']:
+        sum_p2_delta_h += RY_h
+    if p2_terminal_bases in ['YY', 'RR']:
+        sum_p2_delta_h += YY_h
+    if p2_terminal_bases in ['RY', 'YR']:
+        sum_p2_delta_h += RY_h
+    return sum_p2_delta_h
 
-    dh_init = (dh + AT_h if p1_initial_base in ['A', 'T'] else dh + GC_h)
-    ds_init = (ds + AT_s if p1_initial_base in ['A', 'T'] else ds + GC_s)
-
-    dh_term = (dh + AT_h if p1_terminal_base in ['A', 'T'] else dh + GC_h)
-    ds_term = (ds + AT_s if p1_terminal_base in ['A', 'T'] else ds + GC_s)
-
-    dh_total = dh_init + dh_term + sum_p1_delta_h
-    ds_total = ds_init + ds_term + sum_p1_delta_s
-
-    return dh_total, ds_total
-
-def p2_terminal_comp(p2_initial_base, p2_terminal_base, sum_p2_delta_h, sum_p2_delta_s):
-    
-    AT_h, AT_s, GC_h, GC_s = 2.4, 5.7, .45, -1.7 #h in kcal/mol, s in eu
-    dh, ds = 0, 0
-
-    dh_init = (dh + AT_h if p2_initial_base in ['A', 'T'] else dh + GC_h)
-    ds_init = (ds + AT_s if p2_initial_base in ['A', 'T'] else ds + GC_s)
-
-    dh_term = (dh + AT_h if p2_terminal_base in ['A', 'T'] else dh + GC_h)
-    ds_term = (ds + AT_s if p2_terminal_base in ['A', 'T'] else ds + GC_s)
-
-    dh_total = dh_init + dh_term + sum_p2_delta_h
-    ds_total = ds_init + ds_term + sum_p2_delta_s
-
-    return dh_total, ds_total
-
-p1_total_h, p1_total_s = p1_terminal_comp(p1_initial_base, p1_terminal_base, sum_p1_delta_h, sum_p1_delta_s)
-p2_total_h, p2_total_s = p2_terminal_comp(p2_initial_base, p2_terminal_base, sum_p2_delta_h, sum_p2_delta_s)
-
+p1_total_h = p1_end_comp(p1_initial_bases,p1_terminal_bases,sum_p1_delta_h)
+p2_total_h = p2_end_comp(p2_initial_bases,p2_terminal_bases,sum_p2_delta_h)
 ###################################################################################################################################
 #Melting Temperature Calculation and Salt Adjustments
 ###################################################################################################################################
 
 #Determines the melting temperature of the primers
-primer1_melting_temperature = (1000 * p1_total_h) / (p1_total_s + (gas_constant * (math.log(oligo_c)))) - 273.15
-primer2_melting_temperature = (1000 * p2_total_h) / (p2_total_s + (gas_constant * (math.log(oligo_c)))) - 273.15
+primer1_melting_temperature = (1000 * p1_total_h) / (sum_p1_delta_s + (gas_constant * (math.log(oligo_c)))) - 273.15
+primer2_melting_temperature = (1000 * p2_total_h) / (sum_p2_delta_s + (gas_constant * (math.log(oligo_c)))) - 273.15
 
 #Adjustments and unit conversions for the chosen buffer conditions
 Mon = Mono / 2.0 #Divide by two to account for the counterion present, e.g. Cl-, SO4-, etc.
@@ -214,33 +193,25 @@ def primer1_salt_correction(primer1_melting_temperature, primer1_gc, primer1_len
     a, b, c, d, e, f, g = 3.92e-5, -9.11e-6, 6.06e-5, 1.42e-5, -4.82e-4, 5.65e-4, 8.31e-5
     
     if mon == 0:
-
         salt2 = (1 / (primer1_melting_temperature + 273.15)) + a + (b * math.log(mg)) + ((primer1_gc / 100) * (c + d * math.log(mg))) + (1 / (2.0 * (primer1_length - 1))) * (e + f * math.log(mg) + g * math.log(mg) ** 2)
-
         return round((1 / salt2) - 273.15, 1)
 
     R = math.sqrt(mg) / mon 
 
-    if R < 0.22:
-
-        salt1 = (1 / (primer1_melting_temperature + 273.15)) + ((4.59e-5 * (primer1_gc / 100)) - 2.90e-5) * math.log(mon) + 8.81e-6 * (math.log(mon)) ** 2
-
+    if R < 0.22: #Constant values here were also optimized used Chi-square minimization
+        salt1 = (1 / (primer1_melting_temperature + 273.15)) + ((3.087315696964695e-05 * (primer1_gc / 100)) - 2.2560798983637505e-05) * math.log(mon) + 8.081965258030895e-06 * (math.log(mon)) ** 2
         return round((1 / salt1) - 273.15, 1)
 
     elif R < 6.0:
-
         a = 3.92e-5 * (0.843 - (0.352 * math.sqrt(mon) * math.log(mon)))
         d = 1.42e-5 * ((1.279 - 4.03e-3 * math.log(mon)) - 8.03e-3 * (math.log(mon) ** 2))
         g = 8.31e-5 * ((0.486 - 0.258 * math.log(mon)) + 5.25e-3 * (math.log(mon) ** 3)) 
 
         salt2 = (1 / (primer1_melting_temperature + 273.15)) + a + (b * math.log(mg)) + ((primer1_gc / 100) * (c + d * math.log(mg))) + (1 / (2.0 * (primer1_length - 1))) * (e + f * math.log(mg) + g * math.log(mg) ** 2)
-
         return round((1 / salt2) - 273.15, 1)
     
     elif R > 6.0:
-
         salt2 = (1 / (primer1_melting_temperature + 273.15)) + a + (b * math.log(mg)) + ((primer1_gc / 100) * (c + d * math.log(mg))) + (1 / (2.0 * (primer1_length - 1))) * (e + f * math.log(mg) + g * math.log(mg) ** 2)
-
         return round((1 / salt2) - 273.15,1)
 
 def primer2_salt_correction(primer2_melting_temperature, primer2_gc, primer2_length, mg, mon):
@@ -248,47 +219,35 @@ def primer2_salt_correction(primer2_melting_temperature, primer2_gc, primer2_len
     a, b, c, d, e, f, g = 3.92e-5, -9.11e-6, 6.06e-5, 1.42e-5, -4.82e-4, 5.65e-4, 8.31e-5
     
     if mon == 0:
-
         salt2 = (1 / (primer2_melting_temperature + 273.15)) + a + (b * math.log(mg)) + ((primer2_gc / 100) * (c + d * math.log(mg))) + (1 / (2.0 * (primer2_length - 1))) * (e + f * math.log(mg) + g * math.log(mg) ** 2)
-
         return round((1 / salt2) - 273.15, 1)
 
     R = math.sqrt(mg) / mon 
 
     if R < 0.22:
-
-        salt1 = (1 / (primer2_melting_temperature + 273.15)) + ((4.59e-5 * (primer2_gc / 100)) - 2.90e-5) * math.log(mon) + 8.81e-6 * (math.log(mon)) ** 2
-
+        salt1 = (1 / (primer2_melting_temperature + 273.15)) + ((3.087315696964695e-05 * (primer2_gc / 100)) - 2.2560798983637505e-05) * math.log(mon) + 8.081965258030895e-06 * (math.log(mon)) ** 2
         return round((1 / salt1) - 273.15, 1)
 
     elif R < 6.0:
-
         a = 3.92e-5 * (0.843 - (0.352 * math.sqrt(mon) * math.log(mon)))
         d = 1.42e-5 * ((1.279 - 4.03e-3 * math.log(mon)) - 8.03e-3 * (math.log(mon) ** 2))
         g = 8.31e-5 * ((0.486 - 0.258 * math.log(mon)) + 5.25e-3 * (math.log(mon) ** 3)) 
 
         salt2 = (1 / (primer2_melting_temperature + 273.15)) + a + (b * math.log(mg)) + ((primer2_gc / 100) * (c + d * math.log(mg))) + (1 / (2.0 * (primer2_length - 1))) * (e + f * math.log(mg) + g * math.log(mg) ** 2)
-
         return round((1 / salt2) - 273.15, 1)
     
     elif R > 6.0:
-
         salt2 = (1 / (primer2_melting_temperature + 273.15)) + a + (b * math.log(mg)) + ((primer2_gc / 100) * (c + d * math.log(mg))) + (1 / (2.0 * (primer2_length - 1))) * (e + f * math.log(mg) + g * math.log(mg) ** 2)
-
         return round((1 / salt2) - 273.15,1)
 
 adj_primer1_melting_temperature = primer1_salt_correction(primer1_melting_temperature, primer1_gc, primer1_length, mg, mon)
 adj_primer2_melting_temperature = primer2_salt_correction(primer2_melting_temperature, primer2_gc, primer2_length, mg, mon)
-
 ###################################################################################################################################
 #Printing the results
 ###################################################################################################################################
-
 print('The GC content of primer 1 is ' + str(round(primer1_gc, 1)) + '%.' + 
 ' The GC content of primer 2 is ' + str(round(primer2_gc, 1)) + '%.')
 print('The melting temperatures of your primers are ' + str(round(primer1_melting_temperature, 1)) + 
 'C for primer 1, and ' + str(round(primer2_melting_temperature, 1)) + 'C for primer 2.')
 print('The salt adjusted melting temperatures of your primers are ' + str(adj_primer1_melting_temperature) + 
 'C for primer 1, and ' + str(adj_primer2_melting_temperature) + 'C for primer 2.')
-#print(sorted(duo_primer1))
-#print(sorted(duo_primer2))
